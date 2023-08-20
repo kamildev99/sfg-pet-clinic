4658777c8aae0c7e481d21f5bf32d8682de17b8d
Kademilowski <130294458+kamildev99@users.noreply.github.com>
2023-08-20 21:19:48 +0200

    Adding Vet Index Page and Controller. Closes #7.

---
+++ pet-clinic-web/src/main/java/kd/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package kd.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        //returns to vets directory and index html file
+        return "vets/index";
+    }
+}
