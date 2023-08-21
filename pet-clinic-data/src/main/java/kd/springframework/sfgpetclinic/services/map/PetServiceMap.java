60a87186050fbf4af28455f424f4356bdd11d154
Kademilowski <130294458+kamildev99@users.noreply.github.com>
2023-08-21 21:04:40 +0200

    adding bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,10 +2,11 @@ package kd.springframework.sfgpetclinic.services.map;
 
 import kd.springframework.sfgpetclinic.model.Pet;
 import kd.springframework.sfgpetclinic.services.CrudService;
+import kd.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends  AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends  AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package kd.springframework.sfgpetclinic.services.map;
 
 import kd.springframework.sfgpetclinic.model.Vet;
 import kd.springframework.sfgpetclinic.services.CrudService;
+import kd.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
