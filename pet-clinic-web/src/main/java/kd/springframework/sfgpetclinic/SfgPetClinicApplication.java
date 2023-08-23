53cdafd1cdd14e3f690a18ca1ea1a696c52a1476
Kademilowski <130294458+kamildev99@users.noreply.github.com>
2023-08-23 21:46:30 +0200

    Added wro4j and cofnig from spc Closes #24

--- pet-clinic-web/src/main/java/kd/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/kd/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan({"kd.springframework.sfgpetclinic", })
+//@ComponentScan({"kd.springframework.sfgpetclinic"})
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
