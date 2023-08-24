2939db7974ef0618d56443c6a28eef20c66ff31f
Kademilowski <130294458+kamildev99@users.noreply.github.com>
2023-08-24 20:30:31 +0200

    added Pet Type Map Service Impl  Closes #29

---
+++ pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package kd.springframework.sfgpetclinic.services;
+
+import kd.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
