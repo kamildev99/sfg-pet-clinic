f105a68b0e6f41c56112dae1d038889d1f0afdf4
Kademilowski <130294458+kamildev99@users.noreply.github.com>
2023-08-24 20:56:57 +0200

    added Seciality Map and interface Service  Closes #31

---
+++ pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package kd.springframework.sfgpetclinic.services;
+
+import kd.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package kd.springframework.sfgpetclinic.services.map;
+
+import kd.springframework.sfgpetclinic.model.Speciality;
+import kd.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
