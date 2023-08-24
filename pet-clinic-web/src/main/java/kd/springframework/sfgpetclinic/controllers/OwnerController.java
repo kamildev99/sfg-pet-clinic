3819b5d068cece3d72ac260cdfbd87639fdb93a7
Kademilowski <130294458+kamildev99@users.noreply.github.com>
2023-08-24 22:41:29 +0200

    Fixed Broken links Closes #45

--- pet-clinic-web/src/main/java/kd/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kd/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -24,4 +24,12 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
+
+
 }
