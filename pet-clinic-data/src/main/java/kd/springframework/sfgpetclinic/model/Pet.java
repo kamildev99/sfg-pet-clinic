d8b7cd97f6a59b1a1b358867bcc03c4071f66986
KamDev <kamildeb99@dev@gmail.com>
2023-08-27 22:34:53 +0200

    Create Visit and implement entity. Closes #38

--- pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/kd/springframework/sfgpetclinic/model/Pet.java
@@ -3,6 +3,8 @@ package kd.springframework.sfgpetclinic.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="pets")
@@ -22,6 +24,16 @@ public class Pet extends BaseEntity{
     @Column(name ="birth_date")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
