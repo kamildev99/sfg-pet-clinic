314f3a2b0a90013e67d6209be9d7cf03cb798fbf
KamDev <kamildeb99@dev@gmail.com>
2023-08-29 23:00:40 +0200

    upgrade to junit 5 closes #4

--- pet-clinic-web/src/test/java/kd/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/kd/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,15 +1,20 @@
 package kd.springframework.sfgpetclinic;
 
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
+    @Test
+    public void contextLoads(){
 
+    }
 
 }
